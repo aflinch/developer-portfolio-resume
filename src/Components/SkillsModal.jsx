9ec5aec10e8f55ea74fa56f22070530daaed1999
aflinch <alexanderflinchum@gmail.com>
2026-06-03 01:00:42 -0500

    implemented the project cards with the hover effect

---
+++ src/Components/SkillsModal.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createPortal } from 'react-dom';
+import {X} from 'lucide-react';
+import personal from "../data/personal";
+import ProjectCard from "./ProjectCard";
+
+function SkillsModal({ skill, iconComponent, onClose }) {
+    const IconComponent = iconComponent;
+
+    return createPortal(
+       <div className="modal-overlay" onClick={onClose}>
+           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+               <button className="modal-close" onClick={onClose}><X /></button>
+               <div className="modal-body">
+                   <div className="modal-header-section">
+                       <h3>{iconComponent ? <IconComponent /> : null} {skill.title}</h3>
+                   </div>
+                   <div className="skills-progress-bar">
+                       <div className="skills-progress" style={{ width: `${skill.level}%` }}></div>
+                   </div>
+                   <p>{skill.description}</p>
+                   <div className="projects-skill-modal">
+                       {personal?.projects
+                           ?.filter((item) => item.tags?.includes(skill.title))
+                           ?.sort((a, b) => b.id - a.id)
+                           ?.slice(0, 4)
+                           .map((item) => (
+                               <ProjectCard item={item} showTags={false} showDescription={false} showLink={false} className="modal-project-card"/>
+                           ))}
+                   </div>
+               </div>
+           </div>
+       </div>,
+       document.body
+    )
+}
+
+export default SkillsModal;

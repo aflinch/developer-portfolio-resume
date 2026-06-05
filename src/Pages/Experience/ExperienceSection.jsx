f578bc7a6fc138097e36f8081767664b6b68f938
aflinch <alexanderflinchum@gmail.com>
2026-06-05 17:29:45 -0500

    added header information for experience

--- src/Pages/Experience/ExperienceSection.jsx
+++ src/Pages/Experience/ExperienceSection.jsx
@@ -1,9 +1,18 @@
+import React from "react";
 
 export default function ExperienceSection() {
 
     return (
         <section id="experienceSection" className="experience-section">
-
+            <div className="experience-section-content">
+                <div className="experince-container">
+                    <div className="section-heading-row">
+                        <h2 className="experience-section-heading">EXPERIENCE<span className="sub-color">.</span></h2>
+                        <hr className="title-divider" />
+                    </div>
+                    <h3 className="experience-section-title">Knowledge Built <span className="sub-color">Out</span></h3>
+                </div>
+            </div>
         </section>
     )
 }
